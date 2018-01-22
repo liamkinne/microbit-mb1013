0074a7c791d5d2348e333eb9c7cc35618e41fdf2
liamkinne <kinneliam@gmail.com>
2018-01-22 21:45:35 +1100

    added analog mode

--- mb1013.py
+++ mb1013.py
@@ -11,3 +11,19 @@ class mb1013_serial:
 		retrun str(uart.readline())[1:]
 		uart.init(115200)
 		pin1.write_digital(False)
+
+class mb1013_analog:
+	def __init__(self, pin_analog, pin_enable=None):
+		self.pin_analog = pin_analog
+		self.pin_enable = pin_enable
+
+		self.pin_analog.read_analog()
+
+	def get(self):
+		if self.pin_enable != None: pin_enable.write_digital(True)
+
+		reading = self.pin_analog.read_analog() * 5
+
+		if self.pin_enable != None: pin_enable.write_digital(False)
+
+		return reading
